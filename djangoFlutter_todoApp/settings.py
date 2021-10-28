eb3eff88d8676191d47c1dd06743e8a83b585617
nerd-tad <tad000720@gmail.com>
2021-10-28 17:20:58 +0530

    white noise bug fix

--- djangoFlutter_todoApp/settings.py
+++ djangoFlutter_todoApp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -120,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   #this is needed to heroku...
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')                        #this is needed to heroku...
 STATIC_URL = '/static/'
 
 # Default primary key field type
